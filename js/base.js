c013052699a305ab1dc9f5ab22b702217b192c66
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-07-18 16:02:20 +0200

    added prompts with check on the console

--- js/base.js
+++ js/base.js
@@ -1,1 +1,13 @@
-alert("Hello! I am an alert box!!");
+// alert("Hello! I am an alert box!!");
+
+const nome = prompt("Qual'è il tuo nome?");
+
+console.log(nome);
+
+const cognome = prompt("Qual'è il tuo cognome?");
+
+console.log(cognome);
+
+const colore = prompt("Qual'è il tuo colore preferito?");
+
+console.log(colore);
